472f2ba517255bafa0c53981f21146629110fcb0
roman <kaossilat0r.rn@gmail.com>
2015-03-16 17:22:04 +0100

    small fix for a problem that i can't explain yet

--- src/unwinding.c
+++ src/unwinding.c
@@ -17,13 +17,17 @@ void doUnwind(unsigned long address, void* context, struct SampleEvent *buffer) 
 	char buf[512];
 #endif
 	// unwind till first interesting function
+	int status = 1;
 	unsigned long functionStart = getFunctionStart((unsigned long) address);
 	while (functionStart == 0 || functionStart < regionStart || functionStart > regionEnd) {
 #if PRINT_FUNCTIONS
 		unw_get_proc_name(&cursor, buf, sizeof(buf), &offp);
-		printf("ip = %lx \t| %s (ignored)\n", (unsigned long) functionStart, buf);
+		printf("ip = %lx \t| %s (SKIP)\n", (unsigned long) functionStart, buf);
 #endif
-		unw_step(&cursor);
+		if (unw_step(&cursor) < 0) {
+			printf("### skipped unwind method.\n\n");
+			return;
+		}
 		unw_get_reg(&cursor, UNW_REG_IP, &functionStart);
 	}
 
@@ -32,7 +36,6 @@ void doUnwind(unsigned long address, void* context, struct SampleEvent *buffer) 
 	buffer->unwindEvents = (struct StackEvent *) malloc(unwindSteps * sizeof(struct StackEvent));
 
 	unsigned long ip, sp;
-	int status = 1;
 	unsigned long bot = (unsigned long) monitor_stack_bottom();
 	while (status > 0 && unwindSteps != 0) {
 
@@ -52,7 +55,7 @@ void doUnwind(unsigned long address, void* context, struct SampleEvent *buffer) 
 		} else {
 #if  PRINT_FUNCTIONS
 			unw_get_proc_name(&cursor, buf, sizeof(buf), &offp);
-			printf("ip = %lx \t| sp = %lx | %s | %i (ignored)\n", (unsigned long) ip, (unsigned long) sp, buf, unwindSteps);
+			printf("ip = %lx \t| sp = %lx | %s | %i (IGNORE)\n", (unsigned long) ip, (unsigned long) sp, buf, unwindSteps);
 #endif
 		}
 
