b31dbc2b479dd5705c4561d892a8c069a0a13d06
roman <kaossilat0r.rn@gmail.com>
2016-06-13 20:02:27 +0200

    updated toolchain

--- py/toolchain.sh
+++ py/toolchain.sh
@@ -8,7 +8,13 @@ Compiler=${CC:-cc}
 export PGOE_TARGET_EXE=./$Bench-$Phase-$Compiler
 
 UNW_NODES=$PGOE_BASE/out/unw-$Bench-$Phase.txt
+if [ ! -f $UNW_NODES ]; then echo "Cannot find unwound nodes: $UNW_NODES"; exit; fi
 INSTR_NODES=$PGOE_BASE/out/instrumented-$Bench-$Phase.txt
+if [ ! -f $INSTR_NODES ]; then echo "Cannot find unwound nodes: $INSTR_NODES"; exit; fi
+
+if [ ! -s $INSTR_NODES ]; then
+	INSTR_NODES=$PGOE_BASE/wl-dummy.txt # replace empty wl with dummy because of clang bug
+fi
 
 Driver=$LIBSAMPLING_BASE/lib/libsampling.$Compiler.$HOSTNAME.so
 
@@ -20,4 +26,6 @@ WL_FILE=$INSTR_NODES make sel-instr -j 8 # this is just for testing on my laptop
 # create nm_file and regions_file
 python $LIBSAMPLING_BASE/py/gen.py $PGOE_TARGET_EXE.sel $UNW_NODES
 
-ls $Driver
+
+echo "taskset -c 13 monitor-run -i $Driver $PGOE_TARGET_EXE.sel"
+
