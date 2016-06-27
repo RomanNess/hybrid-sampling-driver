1bf32ca8410354a615243b3bb0f1d74b59cb362b
roman <kaossilat0r.rn@gmail.com>
2016-06-27 13:48:19 +0200

    updated flat toolchain

--- py/toolchain-flat.sh
+++ py/toolchain-flat.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
 CUR_DIR=${PWD##*/}
-Bench=${CUR_DIR/spec_cpu./}
+tmp=${PGOE_BENCHMARK:-${CUR_DIR/spec_cpu./}}
+if [[ $tmp == *"povray"* ]]; then
+    Bench=$tmp.gcc
+else
+    Bench=$tmp.clang
+fi
 Compiler=${CC:-cc}
 
 export PGOE_TARGET_EXE=./$Bench-$Compiler
+Target=$PGOE_TARGET_EXE.vanilla
+
+CheckFile() {
+	if [ ! -f $1 ]; then (>&2 echo "Cannot find $2 file: $1"); exit; fi
+}
 
 Driver=$LIBSAMPLING_BASE/lib/libflat.so
-if [ ! -f $Driver ]; then echo "Cannot find driver: $Driver"; exit; fi
+CheckFile $Driver "driver"
 
 # compile target binary
 make vanilla-no-inline -j 8
+CheckFile $Target "target"
 
 rm -f flat_profile
 
-python $LIBSAMPLING_BASE/py/gen.py $PGOE_TARGET_EXE.vanilla
+python $LIBSAMPLING_BASE/py/gen.py $Target
 
-echo "taskset -c 13 monitor-run -i $Driver $PGOE_TARGET_EXE.vanilla $(< ref)"
+echo "taskset -c 13 monitor-run -i $Driver $Target $(< ref)"
 if [ "$1" = "go" ]; then
-	monitor-run -i $Driver $PGOE_TARGET_EXE.vanilla $(< ref)
+	monitor-run -i $Driver $Target $(< ref)
 	python $LIBSAMPLING_BASE/py/flat.py .
+	cp samples_file.txt $PGOE_BASE/spec-centos/$Bench.samples
 fi
 
 if [ "$1" = "t420" ]; then
